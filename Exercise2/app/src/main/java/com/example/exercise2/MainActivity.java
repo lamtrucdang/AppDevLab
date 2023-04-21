b90d0522e738e2d5d22e7ba20343f733ec99ada1
Lam Truc Dang <19521736@gm.uit.edu.vn>
2023-04-21 14:15:09 +0700

    Done Lab2 - Ex2

--- Exercise2/app/src/main/java/com/example/exercise2/MainActivity.java
+++ Exercise2/app/src/main/java/com/example/exercise2/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.exercise2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RadioButton btn_red = findViewById(R.id.btn_red);
+        RadioButton btn_green = findViewById(R.id.btn_green);
+        RadioButton btn_blue = findViewById(R.id.btn_blue);
+        RadioButton btn_gray = findViewById(R.id.btn_gray);
+        LinearLayout bg = findViewById(R.id.bg);
+
+        btn_red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bg.setBackgroundColor(Color.RED);
+            }
+        });
+        btn_green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bg.setBackgroundColor(Color.GREEN);
+            }
+        });
+        btn_blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bg.setBackgroundColor(Color.BLUE);
+            }
+        });
+        btn_gray.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bg.setBackgroundColor(Color.GRAY);
+            }
+        });
     }
 }
